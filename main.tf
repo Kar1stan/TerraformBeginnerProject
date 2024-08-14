e5b3b838303a839fc624335cbc6ff623fece9cb2
Karastan Vladislav <90850632+Kar1stan@users.noreply.github.com>
2024-08-14 17:21:10 +0300

    Update main.tf

--- main.tf
+++ main.tf
@@ -11,7 +11,7 @@ resource "aws_instance" "example" {
   ami           =  "ami-0fb653ca2d3203ac1" 
   instance_type = "t2.micro"
   key_name = "ssh-pair"
-  security_groups = [aws_security_group.web.id]
+  vpc_security_group_ids = [aws_security_group.web.id]
   user_data = <<-EOF
               #!/bin/bash
               echo "Hello, World" > index.html
